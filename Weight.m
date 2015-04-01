e95a1dc6a955796d88a93f13bfc7e35b4be11ee3
Skyler <skymanzanares@hotmail.com>
2015-04-01 16:02:01 -0600

    Did a lot of balancing on average function to use the square of the new image totry and get a more accurate centroid -- accounting for gray creating through resizing

--- Weight.m
+++ Weight.m
@@ -13,7 +13,7 @@ function output=Weight(input)
         files = dir('*.jpg');
         
         %init empty matrix
-        output = zeros(48, 64);
+        output = zeros(48, 64)
         for i=1:length(files)
             filename = files(i).name;
             fprintf('Processing file: %s\n', filename);
@@ -21,30 +21,29 @@ function output=Weight(input)
             %polarize the image
             img = polarize(filename);
             %img matrix is added to output matrix
-            output= output + img;
-            i=i+1;
+            output= (img .* img) + output;
+            %i=i+1;
         end
     end
-    
     %all img matrices should be added, now averaged
    output= output/i;
    output=output*148;  %skews values for proper threshhold
-   output= threshhold(output); %threshhold the avg
+   output= threshhold(output, 127); %threshhold the avg
    %output= (2*output) - 1;
    
    cd(olddir); 
 end
     
     
-    function output=polarize(img)
+function output=polarize(img)
     if (ischar(img))
        img = getImage(img);
     end
     
     img = resizeImage(img);
     
-    output = rgb2gray(img);
-    output = threshhold(output);
+    img = rgb2gray(img);
+    output = threshhold(img, 230);
 end
 
 function img=getImage(filename)
@@ -52,14 +51,15 @@ function img=getImage(filename)
 end
 
 function img=resizeImage(imgIn)
-    img=imresize(imgIn, [48, 64]);
+    img=imresize(imgIn, [48, 64], 'bilinear');
+    %img = imresize(imgIn, [48, 64]);
 end
 
-function output=threshhold(InputMatrix)
+function output=threshhold(InputMatrix, thresh)
     [w,h,d] = size(InputMatrix);
     for i=1:w
     for j=1:h
-       if (InputMatrix(i,j) < 128)
+       if (InputMatrix(i,j) < thresh)
           output(i,j) = 0;
        else
            output(i,j) = 1;
