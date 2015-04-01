6817a7ddd0a7da2ecd8509d02277f10424a08dd6
Skyler <smanzana@nmt.edu>
2015-03-31 23:06:33 -0600

    Fixed slip-up with overwriting bam

--- bam.m
+++ bam.m
@@ -1,21 +1,15 @@
-%Expects a matrix with each COLUMN being a centroid
-function output=ham(inputmatrix)
-    [h,w] = size(inputmatrix);
-    output = zeros(h, h);
-    
-    for i=1:w
-       output = output + getWeight(inputmatrix(:, i))
-    end
-    
-    %go through and zero out the diagonal
-    for i=1:h
-       output(i,i) = 0;
-    end
-    
-    
+%This Function operates correctly when the input_vector is horizontal
+%and the centroid_vector is vertical in structure.
+function output_vector = bam( input_vector, centroid_vector )
+%This function applies a Bidirectional Associative Memory
+%Algorithm against 2 input vectors
+
+%Determined transposed weight
+centroid_trans = centroid_vector';
+%Find the Y neuron layer output
+Y_layer_output = centroid_vector * input_vector;
+%Put the Y output back into a transposed weight vector
+%This is to get back out stored output
+output_vector = Y_layer_output * centroid_trans;
 end
 
-function weightMatrix = getWeight(inputVector)
-   transVect = inputVector';
-   weightMatrix = inputVector * transVect;
-end
--- ham.m
+++ ham.m
@@ -1,10 +1,15 @@
 %Expects a matrix with each COLUMN being a centroid
 function output=ham(inputmatrix)
-    [w,h] = size(inputmatrix(:, 1));
+    [h,w] = size(inputmatrix);
     output = zeros(h, h);
     
     for i=1:w
-       output = output + getWeight(inputmatrix(:, i));
+       output = output + getWeight(inputmatrix(:, i))
+    end
+    
+    %go through and zero out the diagonal
+    for i=1:h
+       output(i,i) = 0;
     end
     
     
@@ -12,5 +17,5 @@ end
 
 function weightMatrix = getWeight(inputVector)
    transVect = inputVector';
-   weightMatrix = transVect * inputVector;
+   weightMatrix = inputVector * transVect;
 end
