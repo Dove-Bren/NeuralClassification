702f0464db0bc9219285f992be67157ac968cf7a
William Fong <trigition@gmail.com>
2015-03-31 23:36:04 -0600

    Added further implementation of the BAM network

--- bam.m
+++ bam.m
@@ -1,13 +1,14 @@
 %This Function operates correctly when the input_vector is horizontal
-%and the centroid_vector is vertical in structure.
-function output_vector = bam( input_vector, centroid_vector )
-%This function applies a Bidirectional Associative Memory
-%Algorithm against 2 input vectors
-
-%Determined transposed weight
-centroid_trans = centroid_vector';
-%Find the Y neuron layer output
-Y_layer_output = centroid_vector * input_vector;
-%Put the Y output back into a transposed weight vector
-%This is to get back out stored output
-output_vector = Y_layer_output * centroid_trans;
+%and the centroid_matrix is arranged vertically.
+function output_matrix = bam( input_vector, centroid_matrix )
+    %This function applies a Bidirectional Associative Memory
+    %Algorithm
+    WeightMatrix;
+    [columns, rows] = size(bam);
+    %Find weights per matrix and find sum
+    for col=1:columns
+        tmpMatrix = col * input_vector;
+        WeightMatrix += tmpMatrix;
+    end
+    
+    output_matrix = WeightMatrix;
